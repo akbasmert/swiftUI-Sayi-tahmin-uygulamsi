e5974491ffbee19d989b20b508fe4247013d04b3
mert <mert.ma346@gmail.com>
2022-10-11 16:02:34 +0300

    Initial Commit

---
+++ sayı tahmin uygulaması/say__tahmin_uygulamas_App.swift
@@ -0,0 +1,17 @@
+//
+//  say__tahmin_uygulamas_App.swift
+//  sayı tahmin uygulaması
+//
+//  Created by Mert AKBAŞ on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct say__tahmin_uygulamas_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
